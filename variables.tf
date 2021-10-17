a6166f39bf2a0034514579771c137a17d9eb7455
Mohammad Faraz <farazm708@gmail.com>
2021-10-17 20:10:44 +0530

    Added missed variable and descriptions

--- variables.tf
+++ variables.tf
@@ -1,23 +1,35 @@
 variable "do_token" {}
 variable "do_api_endpoint" {
+    description = "DigitalOcean API endpoint"
     default     = "https://api.digitalocean.com" 
 }
 variable "cluster_name" {
+    description = "Name of the cluster"
     default     = "kube_cluster"
 }
 variable "is_auto_upgrade" {
+    description = "Is auto upgrade of cluster required ?"
     default     = false
 }
 variable "is_ha" {
+    description = "Is highly available control plane required ?"
     default     = false
 }
 variable "cluster_region" {
+    description = "Region to deploy the cluster in."
     default     = "nyc1"
 }
 variable "worker_name" {
+    description = "Name of the worker node group"
     default     = "kube_workers"
 }
 
 variable "worker_size" {
+    description = "Worker instance size"
     default     = "s-2vcpu-2gb"
+}
+
+variable "worker_count" {
+    description = "Number of workers"
+    default     = 1 
 }
